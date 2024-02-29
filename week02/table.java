32c820a1b5e1f5c8522654c99af6f9172e3d4b02
Swastik-Kunwar <140574565+Swastik-Kunwar@users.noreply.github.com>
2024-02-29 20:03:45 +0545

    Delete week02 directory

--- week02/table.java
+++
@@ -1,9 +0,0 @@
-public class table {
-    public static void main(String args[]){
-        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n==\t\tStudent Points\t\t==\n");
-
-        System.out.println("Name\tLab\tBonus\tTotal\n----/\t---\t---\t----\nJack\t42\t4\t43\nJoe\t30\t3\t54\nHarry\t34\t7\t40\nAdam\t50\t10\t60\nRuss\t46\t6\t45");
-
-    }
-    
-}
